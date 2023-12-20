fe640917e80fd4c29e8b5bac8919e523306ffdb9
S22F5 <51321684+S22F5@users.noreply.github.com>
2023-12-20 11:56:44 +0100

    resolved var conflict & added shebang

--- all/compress-dir.sh
+++ all/compress-dir.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 shopt -s nullglob
-#start conversions
 ##gif
 mogrify -layers 'optimize' -fuzz 7% *.gif
 ##png
@@ -7,14 +7,13 @@ mogrify -quality 50 *.png
 ##jpeg
 mogrify -quality 50 -format png *.jpg
 ##pdf
-for i in *.pdf; do
-	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dAutoRotatePages=/None  -dColorImageDownsampleType=/Bicubic -dColorImageResolution="72" -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution="72" -dMonoImageDownsampleType=/Subsample -dMonoImageResolution="72"  -dNOPAUSE -dQUIET -dBATCH -sOutputFile=tmp-$i $i
-	mv -f tmp-$i $i
+for p in *.pdf; do
+	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dAutoRotatePages=/None  -dColorImageDownsampleType=/Bicubic -dColorImageResolution="72" -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution="72" -dMonoImageDownsampleType=/Subsample -dMonoImageResolution="72"  -dNOPAUSE -dQUIET -dBATCH -sOutputFile=tmp-$p $p
+	mv -f tmp-$p $p
 done
 ##flac
-for i in *.flac; do
-	flac -s -4 --replay-gain "$i" -o "tmp-$i"
-	mv -f "tmp-$i" "$i"
+for f in *.flac; do
+	flac -s -4 --replay-gain "$f" -o "tmp-$f"
+	mv -f "tmp-$f" "$f"
 done
-##done
-echo done
+printf "done\n"
